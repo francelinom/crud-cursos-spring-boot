88f1888b3f081c1a25ee5c4680546ad0cac2c387
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-07-05 14:13:39 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/francelinodev/crudcursosspringboot/CrudCursosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.francelinodev.crudcursosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCursosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCursosSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/francelinodev/crudcursosspringboot/CrudCursosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.francelinodev.crudcursosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCursosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
