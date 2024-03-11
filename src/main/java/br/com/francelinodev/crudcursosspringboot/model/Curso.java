2e18e954df1dfeaf36303d4c77de28f11ab9ce19
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-03-11 20:52:34 -0300

    - Fix: ajustando o validações

--- src/main/java/br/com/francelinodev/crudcursosspringboot/model/Curso.java
+++ src/main/java/br/com/francelinodev/crudcursosspringboot/model/Curso.java
@@ -5,7 +5,9 @@ import br.com.francelinodev.crudcursosspringboot.enums.Status;
 import br.com.francelinodev.crudcursosspringboot.enums.converters.CategoryConverter;
 import br.com.francelinodev.crudcursosspringboot.enums.converters.StatusConverter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -43,6 +45,9 @@ public class Curso {
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ATIVO;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Lesson> lessons = new ArrayList<>();
 }
